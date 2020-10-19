d28336b6ae85f71a3b15e69f464d1a68c4dca0c6
Patrick Wolthausen <pwolthausen@cloudops.com>
2020-10-19 17:03:43 -0400

    added tf to build cluser infra

---
+++ cluster/infra/variables.tfvars
@@ -0,0 +1,6 @@
+api_key      = "pbyxtuQCZmIXsyxZuDF2hA=="
+envID        = "c84782aa-6076-41f0-82fd-fa0056457c3f"
+netID        = "8c610d2e-f3d0-412c-bd27-1e9bff437843"
+template     = "CentOS 8"
+ssh_key_name = "pw-gaming-server-WQ1"
+master_count = 3
---
+++ cluster/infra/vars.tf
@@ -0,0 +1,37 @@
+variable "api_key" {}
+variable "envID" {}
+variable "netID" {}
+variable "template" {}
+variable "ssh_key_name" {}
+
+variable "master_count" {}
+variable "master_cpu_count" {
+  default = 2
+}
+variable "master_mem_count" {
+  default = 4096
+}
+variable "master_disk_size" {
+  default = 50
+}
+variable "master_ip" {
+  type    = list
+  default = ["10.220.124.10", "10.220.124.11", "10.220.124.12"]
+}
+
+variable "worker_count" {
+  default = 3
+}
+variable "worker_cpu_count" {
+  default = 2
+}
+variable "worker_mem_count" {
+  default = 4096
+}
+variable "worker_disk_size" {
+  default = 100
+}
+variable "worker_ip" {
+  type    = list
+  default = ["10.220.124.13", "10.220.124.14", "10.220.124.15", "10.220.124.16", "10.220.124.17", "10.220.124.18", "10.220.124.19"]
+}
